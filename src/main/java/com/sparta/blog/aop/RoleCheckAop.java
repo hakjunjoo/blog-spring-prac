cbc1a8cd669c2388f161ce1ba4bab8169057225f
hakjunjoo <hakjun0918@nate.com>
2023-07-28 17:15:08 +0900

    댓글 수정 및 삭제 권환 확인 AOP 적용

--- src/main/java/com/sparta/blog/aop/RoleCheckAop.java
+++ src/main/java/com/sparta/blog/aop/RoleCheckAop.java
@@ -1,11 +1,14 @@
 package com.sparta.blog.aop;
 
 import com.sparta.blog.entity.Blog;
+import com.sparta.blog.entity.Comment;
 import com.sparta.blog.entity.User;
 import com.sparta.blog.entity.UserRoleEnum;
 import com.sparta.blog.exception.blog.NoExistBlogException;
+import com.sparta.blog.exception.blog.NoExistCommentException;
 import com.sparta.blog.exception.blog.NoPermissionException;
 import com.sparta.blog.repository.BlogRepository;
+import com.sparta.blog.repository.CommentRepository;
 import com.sparta.blog.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -19,12 +22,19 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RoleCheckAop {
     private final BlogRepository blogRepository;
+    private final CommentRepository commentRepository;
 
     @Pointcut("execution(* com.sparta.blog.service.BlogService.updateBlog(..))")
     private void updateBlog() {}
 
     @Pointcut("execution(* com.sparta.blog.service.BlogService.deleteBlog(..))")
     private void deleteBlog() {}
+
+    @Pointcut("execution(* com.sparta.blog.service.CommentService.updateComment(..))")
+    private void updateComment() {}
+
+    @Pointcut("execution(* com.sparta.blog.service.CommentService.deleteComment(..))")
+    private void deleteComment() {}
 
     @Around("updateBlog() || deleteBlog()")
     public Object blogRoleCheck(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -38,6 +48,26 @@ public class RoleCheckAop {
 
         if (!user.getRole().equals(UserRoleEnum.ADMIN) || blog.getAuthor().equals(user.getUsername())) {
             throw new NoPermissionException("본인이 작성한 게시글만 수정/삭제할 수 있습니다.");
+        }
+
+        return joinPoint.proceed();
+    }
+
+    @Around("updateComment() || deleteComment()")
+    public Object commentRoleCheck(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 댓글 정보 받아오기
+        Long blogId = (Long) joinPoint.getArgs()[0];
+        Long commentId = (Long) joinPoint.getArgs()[1];
+        Comment comment = commentRepository.findByIdAndBlogId(commentId, blogId);
+
+        // 로그인된 유저 정보 받아오기
+        UserDetailsImpl userDetails = (UserDetailsImpl) joinPoint.getArgs()[2];
+        User user = userDetails.getUser();
+
+        if(comment == null) throw new NoExistCommentException("해당 댓글이 존재하지 않습니다.");
+
+        if (!(user.getRole().equals(UserRoleEnum.ADMIN) || comment.getUsername().equals(user.getUsername()))) {
+            throw new NoPermissionException("자신이 등록한 댓글만 수정/삭제할 수 있습니다.");
         }
 
         return joinPoint.proceed();
--- src/main/java/com/sparta/blog/service/CommentServiceImpl.java
+++ src/main/java/com/sparta/blog/service/CommentServiceImpl.java
@@ -4,10 +4,7 @@ import com.sparta.blog.dto.CommentRequestDto;
 import com.sparta.blog.dto.CommentResponseDto;
 import com.sparta.blog.entity.Blog;
 import com.sparta.blog.entity.Comment;
-import com.sparta.blog.entity.UserRoleEnum;
 import com.sparta.blog.exception.blog.NoExistBlogException;
-import com.sparta.blog.exception.blog.NoExistCommentException;
-import com.sparta.blog.exception.blog.NoPermissionException;
 import com.sparta.blog.repository.BlogRepository;
 import com.sparta.blog.repository.CommentRepository;
 import com.sparta.blog.security.UserDetailsImpl;
@@ -36,26 +33,14 @@ public class CommentServiceImpl implements CommentService{
 	@Override
 	@Transactional
 	public void updateComment(Long blogId, Long commentId,  UserDetailsImpl userDetails, CommentRequestDto requestDto) {
-		Comment comment = checkComment(blogId, commentId, userDetails);
+		Comment comment = commentRepository.findByIdAndBlogId(commentId, blogId);
 		comment.update(requestDto);
 	}
 
 	//댓글 삭제 API
 	@Override
 	public void deleteComment(Long blogId, Long commentId, UserDetailsImpl userDetails) {
-		Comment comment = checkComment(blogId, commentId, userDetails);
-		commentRepository.delete(comment);
-	}
-
-	//수정 또는 삭제할 블로그가 존재하는 지 확인하고 게시글을 수정 또는 삭제할 수 있는 회원인지 확인하는 메서드
-	private Comment checkComment(Long blogId,  Long commentId, UserDetailsImpl userDetails) {
 		Comment comment = commentRepository.findByIdAndBlogId(commentId, blogId);
-		if(comment == null) throw new NoExistCommentException("해당 댓글이 존재하지 않습니다.");
-
-		if (!(userDetails.getUser().getRole().equals(UserRoleEnum.ADMIN) || comment.getUsername().equals(userDetails.getUsername()))) {
-			throw new NoPermissionException("자신이 등록한 댓글만 수정/삭제할 수 있습니다.");
-		}
-
-		return comment;
+		commentRepository.delete(comment);
 	}
 }
